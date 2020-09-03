c726e7a4533ed6cee20497d2da659dc29d838428
alamrumambi <alam.abumaryam@gmail.com>
2020-09-03 12:08:24 +0700

    refactor if have son fix if have grandson fix if have daughter & granddaughter refactor constructor of class beneficiary

--- Models/beneficiaryModel.js
+++ Models/beneficiaryModel.js
@@ -1,16 +1,21 @@
-class Model {
+class Penerima_Waris {
     constructor (name, got, nop) {
-        this.name = name,
-        this.got = got,
-        this.funds = 0,
-        this.numberOfPeople = nop,
-        this.perPerson = this.funds/ nop
+        this.nama = name,
+        this.bagian_hukum_waris = got,
+        this.jumlah_orang = nop,
+        this.bagian_terhitung = '',
+        this.dana_didapat = 0,
+        this.dana_per_orang = this.dana_didapat/ nop
     }
 
     updateFunds(value) {
-        this.funds = Number(value.toFixed(2));
-        this.perPerson = Number((value/ this.numberOfPeople).toFixed(2));
+        this.dana_didapat = Number(value.toFixed(2));
+        this.dana_per_orang = Number((value/ this.jumlah_orang).toFixed(2));
+    }
+
+    updateCountPart(value) {
+        this.bagian_terhitung = value;
     }
 }
 
-module.exports = Model;
+module.exports = Penerima_Waris;
